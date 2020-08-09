15b621baf860a2b02ff51e6d9bfe458f5a84dcc5
oabrivard <olivier@abrivard.fr>
2020-08-09 11:25:34 -0400

    updated notes-backend

---
+++ build/precache-manifest.34e6a8538a84a7c3b6debbf9c0881384.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5dc16736c642df9bdf93c96a358eb3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ae85dfb3afc4373589",
+    "url": "/static/css/main.aa30d9df.chunk.css"
+  },
+  {
+    "revision": "81577c2581627fd4ba39",
+    "url": "/static/js/2.236b0b3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.236b0b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ae85dfb3afc4373589",
+    "url": "/static/js/main.c7502447.chunk.js"
+  },
+  {
+    "revision": "420e90d25d756f6d8af3",
+    "url": "/static/js/runtime-main.e56cac76.js"
+  }
+]);
